a04d50e8fa47636e5b9f3a08037143b2f91417e3
S. Monty Suwannukul <monty@pivotallabs.com>
2015-03-10 16:47:08 -0700

    new design

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   get '/complete', to: 'home#complete'
   get '/chainfile', to: 'home#chainfile'
   get '/existing', to: 'home#existing'
+  get '/cert-uploaded', to: 'home#cert-uploaded'
+  get '/txt-cert-uploaded', to: 'home#txt-cert-uploaded'
+  get '/key-uploaded', to: 'home#key-uploaded'
+  get '/cert-key-uploaded', to: 'home#cert-key-uploaded'
+  get '/progress', to: 'home#progress'
 end
